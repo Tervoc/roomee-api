ae1c2957540b40a806dac178a1d62caf12ac858f
Christian Parrish <christian.parrish@ttu.edu>
2021-02-15 20:25:42 -0600

    Basic project setup
    basic project setup

---
+++ roomee-api/roomee-api/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace roomee_api
+{
+    public class Program {
+        public static void Main(string[] args) {
+            var host = new HostBuilder()
+                .ConfigureAppConfiguration((hostContext, builder) => {
+                    builder.AddJsonFile("appsettings.json");
+                }).ConfigureWebHostDefaults(webBuilder => {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .Build();
+
+            host.Run();
+            
+        }       
+    }
+}
---
+++ roomee-api/roomee-api/Startup.cs
@@ -0,0 +1,75 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace roomee_api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers().AddNewtonsoftJson();
+
+            services.AddCors(options => {
+                options.AddPolicy("default",
+                    builder => {
+                        builder.WithOrigins("http://localhost:3000/")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                    });
+            });
+
+            services.AddMvc();
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "roomee API",
+                    Version = "v1",
+                    Contact = new OpenApiContact { Name = "Christian Parrish", Email = "christian.parrish@ttu.edu" },
+                    Description = "API for use by roomee clients."
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors("default"); //cross origin resource sharing 
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "roomee API v1");
+            });
+        }
+    }
+}
