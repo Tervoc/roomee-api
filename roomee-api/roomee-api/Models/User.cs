f9d02d444d3e871e34ab9b6f1952534f77af1271
Christian Parrish <christian.parrish@ttu.edu>
2021-02-17 20:48:50 -0600

    Setup user controller

---
+++ roomee-api/roomee-api/Models/User.cs
@@ -0,0 +1,120 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Security.Claims;
+
+namespace roomee_api.Models {
+	public class User {
+		[JsonProperty(PropertyName = "userId")]
+		public int UserId { get; }
+
+		[JsonProperty(PropertyName = "firstName")]
+		public string FirstName { get; set; }
+
+		[JsonProperty(PropertyName = "lastName")]
+		public string LastName { get; set; }
+
+		[JsonProperty(PropertyName = "preferredName")]
+		public string PreferredName { get; set; }
+
+		[JsonProperty(PropertyName = "password")]
+		public string Password { get; set; }
+
+		[JsonProperty(PropertyName = "email")]
+		public string Email { get; set; }
+
+		[JsonProperty(PropertyName = "statusId")]
+		public int StatusId { get; }
+
+		public static readonly string[] UpdateNames = { "firstName", "lastname", "preferredName", "password", "email", "statusId" };
+
+		public User(int userId, string firstName, string lastName, string preferredName, string password, string email, int statusId) {
+			UserId = userId;
+			FirstName = firstName;
+			LastName = lastName;
+			PreferredName = preferredName;
+			Password = password;
+			Email = email;
+			StatusId = statusId;
+		}
+
+		public static User FromUserId(int userId) {
+			using (SqlConnection conn = new SqlConnection(Startup.ConnectionString)) {
+				conn.Open();
+
+				SqlCommand command = new SqlCommand(@"SELECT * FROM [User] WHERE UserId = @param1;", conn);
+				command.Parameters.AddWithValue("@param1", userId);
+
+				using (SqlDataReader reader = command.ExecuteReader()) {
+					if (reader.HasRows) {
+						reader.Read();
+
+						string prefName = string.Empty;
+
+						if (!reader.IsDBNull(3)) {
+							prefName = reader.GetString(3);
+						}
+
+						return new User(
+							reader.GetInt32(0),
+							reader.GetString(1),
+							reader.GetString(2),
+							prefName,
+							reader.GetString(4),
+							reader.GetString(5),
+							reader.GetInt32(6)
+						);
+					} else {
+						return null;
+					}
+				}
+			}
+		}
+
+		public static User FromEmail(string email) {
+			using (SqlConnection conn = new SqlConnection(Startup.ConnectionString)) {
+				conn.Open();
+
+				SqlCommand command = new SqlCommand(@"SELECT * FROM [User] WHERE Email = @param1;", conn);
+				command.Parameters.AddWithValue("@param1", email);
+
+				using (SqlDataReader reader = command.ExecuteReader()) {
+					if (reader.HasRows) {
+						reader.Read();
+
+						string prefName = string.Empty;
+
+						if (!reader.IsDBNull(3)) {
+							prefName = reader.GetString(3);
+						}
+
+						return new User(
+							reader.GetInt32(0),
+							reader.GetString(1),
+							reader.GetString(2),
+							prefName,
+							reader.GetString(4),
+							reader.GetString(5),
+							reader.GetInt32(6)
+						);
+					} else {
+						return null;
+					}
+				}
+			}
+		}
+
+		public List<Claim> ToClaims() {
+			return new List<Claim> {
+				new Claim("userId", UserId.ToString()),
+				new Claim("email", Email),
+				new Claim("firstName", FirstName),
+				new Claim("lastName", LastName),
+				new Claim("preferredName", PreferredName),
+				new Claim("statusId", StatusId.ToString())
+			};
+		}
+
+	}
+}
--- roomee-api/roomee-api/Startup.cs
+++ roomee-api/roomee-api/Startup.cs
@@ -11,65 +11,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace roomee_api
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+namespace roomee_api {
+	public class Startup {
+		public static string ConnectionString { get; private set; }
+		public static string JWTSecret { get; private set; }
 
-        public IConfiguration Configuration { get; }
+		public Startup(IConfiguration configuration) {
+			Configuration = configuration;
+		}
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers().AddNewtonsoftJson();
+		public IConfiguration Configuration { get; }
 
-            services.AddCors(options => {
-                options.AddPolicy("default",
-                    builder => {
-                        builder.WithOrigins("http://localhost:3000/")
-                        .AllowAnyHeader()
-                        .AllowAnyMethod();
-                    });
-            });
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services) {
+			services.AddControllers().AddNewtonsoftJson();
 
-            services.AddMvc();
-            services.AddSwaggerGen(c => {
-                c.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Title = "roomee API",
-                    Version = "v1",
-                    Contact = new OpenApiContact { Name = "Christian Parrish", Email = "christian.parrish@ttu.edu" },
-                    Description = "API for use by roomee clients."
-                });
-            });
-        }
+			services.AddCors(options => {
+				options.AddPolicy("default",
+					builder => {
+						builder.WithOrigins("http://localhost:3000/")
+						.AllowAnyHeader()
+						.AllowAnyMethod();
+					});
+			});
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+			services.AddMvc();
+			services.AddSwaggerGen(c => {
+				c.SwaggerDoc("v1", new OpenApiInfo {
+					Title = "roomee API",
+					Version = "v1",
+					Contact = new OpenApiContact { Name = "Christian Parrish", Email = "christian.parrish@ttu.edu" },
+					Description = "API for use by roomee clients."
+				});
+			});
+		}
 
-            app.UseCors("default"); //cross origin resource sharing 
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+			if (env.IsDevelopment()) {
+				app.UseDeveloperExceptionPage();
+			}
 
-            app.UseHttpsRedirection();
+			ConnectionString = Configuration.GetConnectionString("RoomeeConnStr");
+			JWTSecret = Configuration.GetValue<string>("JWTSecret");
 
-            app.UseRouting();
+			app.UseCors("default"); //cross origin resource sharing is fucking stupid --rip(matt <3 )
 
-            app.UseEndpoints(endpoints => {
-                endpoints.MapControllers();
-            });
+			app.UseHttpsRedirection();
 
-            app.UseSwagger();
-            app.UseSwaggerUI(c => {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "roomee API v1");
-            });
-        }
-    }
+			app.UseRouting();
+
+			app.UseEndpoints(endpoints => {
+				endpoints.MapControllers();
+			});
+
+			app.UseSwagger();
+			app.UseSwaggerUI(c => {
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "roomee API v1");
+			});
+		}
+	}
 }
