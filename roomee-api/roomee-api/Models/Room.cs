bf6cc0a19ae5aa053937130395cec1a194cd36c6
Christian Parrish <christian.parrish@ttu.edu>
2021-03-01 22:36:01 -0600

    Room model fix
    removed ToClaims function from room model in relation to Room Class issue #3

--- roomee-api/roomee-api/Models/Room.cs
+++ roomee-api/roomee-api/Models/Room.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Security.Claims;
-//using roomee_api.Models;
 
 namespace roomee_api.Models {
 	public class Room {
@@ -12,19 +10,15 @@ namespace roomee_api.Models {
 
 		[JsonProperty(PropertyName = "roomName")]
 		public string RoomName { get; set; }
-		/*
-		[JsonProperty(PropertyName = "roomTag")]
-		public string RoomTag { get; }*/
 
 		[JsonProperty(PropertyName = "statusId")]
 		public int StatusId { get; }
 
 		public static readonly string[] UpdateNames = { "roomName",  "statusId" };
 
-		public Room(int roomId, string roomName, /*string roomTag,*/ int statusId) {
+		public Room(int roomId, string roomName, int statusId) {
 			RoomId = roomId;
 			RoomName = roomName;
-			//RoomTag = roomTag;
 			StatusId = statusId;
 		}
 
@@ -42,7 +36,6 @@ namespace roomee_api.Models {
 						return new Room(
 							reader.GetInt32(0),
 							reader.GetString(1),
-							//reader.GetString(2),
 							reader.GetInt32(2)
 						);
 					} else {
@@ -94,15 +87,5 @@ namespace roomee_api.Models {
 				}
 			}
 		}
-
-		public List<Claim> ToClaims() {
-			return new List<Claim> {
-				new Claim("roomId", RoomId.ToString()),
-				new Claim("roomName", RoomName),
-				//new Claim("roomTag", RoomTag),
-				new Claim("statusId", StatusId.ToString())
-			};
-		}
-
 	}
 }
